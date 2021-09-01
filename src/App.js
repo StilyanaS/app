81f4f0233f335e3a24ccd310a11c11fec1c72752
Estela <USER@MacBook-Pro.local>
2021-09-01 10:02:08 +0200

    Contador con boton

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import  CartWidget from './components/CartWidget/CartWidget';
 
 function App() {
 const [count, setCount] = useState(0);
+const [date, setDate] = useState(Date());
 const handlerClick = () => {
   setCount(count + 1);
+  setDate(Date());
 };
 
 
@@ -16,6 +18,8 @@ const handlerClick = () => {
     <div className="App">
     <NavBar><CartWidget /></NavBar>
     <div><Header /></div>
+    <label>{count}</label><br />
+    <label>{date}</label><br />
     <button onClick={handlerClick}>click</button>
     </div>
   );
