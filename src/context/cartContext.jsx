50ed01e175fd61c532393ed527d7286866fb0738
Estela <USER@MacBook-Pro.local>
2021-09-15 23:58:28 +0200

    CartContext

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -9,10 +9,26 @@ export const cartContext = createContext();
 export const CartContextProvider = ({children}) => {
     const  [cart, setCart] = useState([]);
     const addItem = (item, quantity) => {
+        if (isInCart(item.id)) {
+            const updateCart = [...cart];
+            updateCart.forEach((element)=> {
+                if (element.item.id === item.id) {
+                    element.quantity = element.quantity + quantity;
+                }
+            })
+            setCart(updateCart);
+        } else {
         setCart([...cart,{item, quantity}]);
+        }
     }
+    const isInCart = () => {
+        return cart.find(element=> element.item.id===id)
+    }
+
+    const clear = () => setCart([]);
+
     return (
-        <CartContext.Provider value={custData}>
+        <CartContext.Provider value={addItem}>
             {children}
         </CartContext.Provider>
     )
